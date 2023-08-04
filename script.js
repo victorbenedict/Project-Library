81c737aba593996811e83ea80b7bd8a3e671eb93
Nasuri <victorbenedict.bulaong@gmail.com>
2023-08-04 23:03:03 +0800

    Refractor design and script

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
+const cl = console.log;
+cl('initialize', new Date())
+
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-  // the constructor...
+
+const form = document.getElementById('form_book');
+const tablebody = document.getElementById('tablebody_book');
+
+(function loadPremadeObjects(){
+  myLibrary.push(new Book('A Song of Ice and Fire', 'George R. R. Martin', '694', false));
+  myLibrary.push(new Book('Eat That Frog!', 'Brian Tracy ', '144', true));
+  myLibrary.push(new Book('The Subtle Art of Not Giving a Fuck', 'Mark Manson', '272', true));
+  cl(myLibrary);
+  
+})();
+
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead;
-}
-
-Book.prototype.toggleRead = function (){
-  this.isRead = !this.isRead;
-
-};
-
-function toggleRead(index){
-  myLibrary[index].toggleRead();
-  renderLibrary();
+  this.status = status;
 }
 
 function addBookToLibrary() {
-  //Retrieve the input values
-  let title = document.getElementById('input_title').value;
-  let author = document.getElementById('input_author').value;
-  let pages = document.getElementById('input_pages').value;
-  let isRead = document.getElementById('input_isRead').checked;
-  let newBook = new Book(title,author,pages,isRead);
+  const title =  document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const status = document.getElementById('status').value; 
+  const newBook = new Book(title, author, pages, status);
   myLibrary.push(newBook);
+  cl(myLibrary)
 }
 
-function renderLibrary() {
-  //Process and display the mylibrary array
-  let content_library = document.querySelector('.content_library');
-  content_library.innerHTML = "";
-  for(let i = 0; i < myLibrary.length; i++){
-    let book = myLibrary[i];
-    let showBook = document.createElement('div');
-    showBook.innerHTML = `
-      <p>${book.title} | ${book.author} | ${book.pages} | ${book.isRead ? "Yes" : "No"}
-      <button class="btn_removeBook" onclick="removeBook(${i})">Remove</button>
-      <button class="btn_toggleRead" onclick="toggleRead(${i})">Toggle Read</button></p>`;
-    content_library.appendChild(showBook);
-  };
-}
+function render() {
+  tablebody.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    cl(book,index)
+    const htmlBook = `
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td><button id="btn_status book-${index}">${book.status == true ? 'Read':'Not Read'}</button></td>
+        <td><button id="btn_delete book-${index}">Delete</button></td>
+      </tr>
+      `;
+           tablebody.insertAdjacentHTML("afterbegin", htmlBook);
+    cl('render')
+  });
+};
 
-function removeBook(index){
-  myLibrary.splice(index,1);
-  renderLibrary();
-}
-
-//Displays the form_newBook when clicking the btn_newBook
-document.querySelector('.btn_newBook').addEventListener('click', () => {
-  document.querySelector('.form_newBook').style.display = 'inline';
-});
-
-//Submit button of form_newBook
-document.querySelector('.form_newBook').addEventListener('submit', () => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
+  cl('submit');
   addBookToLibrary();
-  renderLibrary();
+  render();
 });
+
+render();
