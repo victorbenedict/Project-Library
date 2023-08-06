f148e092f8844f0265e9c27e9a2a88d1deb95b97
Nasuri <victorbenedict.bulaong@gmail.com>
2023-08-06 22:20:52 +0800

    Add delete and read feature

--- script.js
+++ script.js
@@ -26,28 +26,34 @@ function addBookToLibrary() {
   const title =  document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
-  const status = document.getElementById('status').value; 
+  const status = document.getElementById('status').value == 'Read' ? true:false; 
   const newBook = new Book(title, author, pages, status);
   myLibrary.push(newBook);
   cl(myLibrary)
+  cl(newBook)
 }
 
 function render() {
+  
   tablebody.innerHTML = "";
   myLibrary.forEach((book, index) => {
+    const statusText = `${book.status == true ? 'Read':'Not Read'}`
+    const statusClass = `${book.status == true ? 'Read':'NotRead'}`
+
     cl(book,index)
     const htmlBook = `
       <tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
-        <td><button id="btn_status book-${index}">${book.status == true ? 'Read':'Not Read'}</button></td>
-        <td><button id="btn_delete book-${index}">Delete</button></td>
+        <td><button class="btn_status ${statusClass}" onClick = "changeStatus(${index})">${statusText}</button></td>
+        <td><button class="btn_delete" onClick = "deleteBook(${index})">Delete</button></td>
       </tr>
       `;
            tablebody.insertAdjacentHTML("afterbegin", htmlBook);
     cl('render')
   });
+  
 };
 
 form.addEventListener('submit', (event) => {
@@ -56,5 +62,20 @@ form.addEventListener('submit', (event) => {
   addBookToLibrary();
   render();
 });
+
+
+function changeStatus(index){
+  myLibrary[index].status = myLibrary[index].status == true ? false:true;
+  render();
+}
+
+function deleteBook(index){
+  let text = `Are you sure to delete "${myLibrary[index].title}"?`;
+  if (confirm(text) == true) {
+    myLibrary.splice(index, 1);
+    render();
+  } 
+  
+}
 
 render();
